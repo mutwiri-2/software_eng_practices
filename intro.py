9f7c6e0e85e8bdb3904b8342ee66ee658709c127
mutwiri-2 <devmutwiri2@gmail.com>
2020-06-08 12:31:52 +0300

    techniques for production-level code

---
+++ intro.py
@@ -0,0 +1,21 @@
+print(r'''
+As Software Engineers, there are a few techniques that we can adopt that will 
+help us to implement production-level solutions.
+
+Production-level solutions or production quality code is code that meets 
+expectations in reliability, efficiency among other for production. This
+production-level code is most times run on production servers.
+
+Software running on production servers handles live users and data of the
+intended audience of our software product.
+
+Techniques:
+1. Write clean and modular code
+2. Improve code efficiency
+3. Document effectively - Add effective documentation
+4. Use version control
+5. Testing
+6. Logging
+7. Code reviews
+
+''')
