7bb90830ab3404cbaf77b35c596ab39dabb5a33b
mutwiri-2 <devmutwiri2@gmail.com>
2020-06-08 16:19:14 +0300

    criteria for production quality code

---
+++ clean_and_modular/clean_modular_code.py
@@ -0,0 +1,15 @@
+print(r'''
+When using s/w products like Google, Amazon, Udacity e.t.c, the code that is
+running the servers you are using is production code.
+
+Ideally, code that is being used in production should meet a number of criteria
+to ensure reliability and efficiency before it becomes public:
+
+1. Code needs to be clean - code is clean when it is READABLE, SIMPLE AND CONCISE
+(brief, yet including all important information). This is a characteristic of
+production quality code which is crucial for collaboration an maintainability in
+s/w development
+clean code makes it easier for others and yourself to understand and reuse code
+
+2. 
+''')
