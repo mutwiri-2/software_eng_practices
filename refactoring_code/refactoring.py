0fe6339247e6b5b23084a06904055514e1f2c3ba
mutwiri-2 <devmutwiri2@gmail.com>
2020-06-08 16:35:30 +0300

    refactoring code

---
+++ refactoring_code/refactoring.py
@@ -0,0 +1,7 @@
+print(r'''
+Refactoring means restructuring code to improve the internal structure without
+changing the external functionality
+
+This gives you a chance to clean and nodularize (create functions to encapsulate)
+your program after you have got it working
+''')
