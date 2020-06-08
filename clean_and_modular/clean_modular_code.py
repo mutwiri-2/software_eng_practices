08fca3befab3122c95adffb91b6a9108bc30d5cf
mutwiri-2 <devmutwiri2@gmail.com>
2020-06-08 16:31:01 +0300

    code should be modular

--- clean_and_modular/clean_modular_code.py
+++ clean_and_modular/clean_modular_code.py
@@ -11,5 +11,19 @@ production quality code which is crucial for collaboration an maintainability in
 s/w development
 clean code makes it easier for others and yourself to understand and reuse code
 
-2. 
+2. Code should be modular - This means your code is logically broken down into
+functions and modules which makes your code more organized, efficient and reusable
+You can encapsulate code in a function and reuse it by calling the function in
+different places in your code
+
+module - In programming, a module is a file. Modules allow code to be reused by
+encapsulating them into files that can be imported into other files
+
+Splitting your code into logical functions and modules allow you to quickly find
+relevant pieces of code
+You can often generalize these pieces of code to be reused in different places
+to prevent yourself from writing unnecessary lines of code
+
+Abstracting out details into functions and modules can really help in improving
+the readability of your code.
 ''')
