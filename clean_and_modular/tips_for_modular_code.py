b915944e0464ae9775f2ea2784a25bc8fdcc5843
mutwiri-2 <devmutwiri2@gmail.com>
2020-06-19 11:53:45 +0300

    DRY - Don't Repeat Yourself!

---
+++ clean_and_modular/tips_for_modular_code.py
@@ -0,0 +1,13 @@
+print(r'''
+Modularization splits your code into logical functions and modules. This allows
+you to reuse parts of your code
+
+A few guidelines for Modularization:
+1. DRY (Don't Repeat Yourself!) - Generalize and consolidate repeated code in
+loops or functions
+2. Abstract out logic to improve readability - Abstracting out code into a
+function not only makes it less repetitive but also improves the readability
+with descriptive function names. Although your code can become more readable 
+when you abstract out logic into functions, it is possible to over-engineer this
+and have way too many modules, so use your judgement.
+''')
